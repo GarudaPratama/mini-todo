2f97ebd286ff1d9ea46d2e8efdbbc645aa23dfca
GarudaPratama <garuda281209@student.abudzar.sch.id>
2026-04-22 10:26:49 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'; // Import Provider
-import { store } from './store';       // Import store yang sudah kita buat
+import { Provider } from 'react-redux'; 
+import { store } from './store';       
 import App from './App';
-import './index.css';                  // Pastikan Tailwind v4 di-import di sini
-
+import './index.css';                  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Bungkus App dengan Provider dan masukkan store sebagai props */}
+    
     <Provider store={store}>
       <App />
     </Provider>
